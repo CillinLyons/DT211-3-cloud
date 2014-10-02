10557a9132d6445858a111f357c8f62536a7ec50
cillin <cillinlyons@outlook.com>
2014-10-02 14:12:36 +0000

    v1

---
+++ solution5.py
@@ -0,0 +1,15 @@
+#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any #remainder.
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+numbers = 0;
+i = 0;
+
+for (numbers = 0; numbers < 10000000; numbers++;)
+{
+  for (i = 0; i < 20; i++)
+  {
+    if (i % numbers = 0 )
+    {
+      
+    }
+  }
+}
